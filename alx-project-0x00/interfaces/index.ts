c05531dbd7de57604f4850c9e51bda997a573513
Brian Maleek <maleekmalik@gmail.com>
2025-09-19 19:55:29 +0300

    Add ButtonProps interface to define properties for Button component

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,11 @@
 export interface PillProps {
     title: string;
 }
+
+export interface ButtonProps {
+    title: string;
+    styles?: string;
+    size?: 'small' | 'medium' | 'large';
+    rounded?: 'sm' | 'md' | 'full';
+    onClick?: () => void;
+}
