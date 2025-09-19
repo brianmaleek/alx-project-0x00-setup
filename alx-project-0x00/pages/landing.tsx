bc1c3c91c47095eaca9aea391b518b4681265048
Brian Maleek <maleekmalik@gmail.com>
2025-09-19 19:55:33 +0300

    Refactor Landing component layout for improved structure and readability

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,23 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
     return (
         <div>
-            <h1 className=" text-xl font-extralight">Landing Page</h1>
-            <Card />
+            <div>
+                <h1 className=" text-xl font-extralight">Landing Page</h1>
+                <Card />
+            </div>
+            <div className="space-x-4 my-3">
+                <Button title="small" styles="text-sm px-2 py-1 rounded-md" />
+                <Button title="medium" styles="text-md px-4 py-2 rounded-lg" />
+                <Button title="large" styles="text-lg px-6 py-3 rounded-full" />
+            </div>
+            <div className="space-x-4 mt-6">
+                <Button title="Rounded SM" styles="px-4 py-2 rounded-sm" />
+                <Button title="Rounded MD" styles="px-4 py-2 rounded-md" />
+                <Button title="Rounded FULL" styles="px-4 py-2 rounded-full" />
+            </div>
         </div>
     )
 }
